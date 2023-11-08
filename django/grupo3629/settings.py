452aa8ee1fcc9b75a3c6c8a7cc89eab9c2696625
JosueTorres2108 <josuett2108@gmail.com>
2023-11-08 10:35:22 -0500

    clase 8/11/2023

--- django/grupo3629/settings.py
+++ django/grupo3629/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios.apps.UsuariosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,6 +119,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR
 STATICFILES_DIRS = ['grupo3629/static']
+
+MEDIA_ROOT =BASE_DIR / 'media'
+MEDIA_URL ='/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
